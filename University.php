dbf3d34dbcd50218829987c0c43ca7a879dc825b
gokcesefa-jotform <gokcesefa@jotform.com>
2021-03-11 00:52:47 +0300

    initial commit

---
+++ University.php
@@ -0,0 +1,78 @@
+<?php
+require_once "AbstractUniversity.php";
+require_once "Subject.php";
+
+class University extends AbstractUniversity
+{
+
+	/*this method return true if the subject does not exist.*/
+	public function controlSubject(string $code, string $name){
+
+		if(count($this->subjects) == 0) return true;
+        foreach($this->subjects as $sub){
+            if ($sub->getCode() == $code && $sub->getName() == $name){
+                return false;
+            }
+        }
+        return true;
+	}
+
+    public function addSubject(string $code, string $name){
+
+    	if($this->controlSubject($code,$name)){
+   
+			$sub = new Subject($code,$name);
+	        $this->subjects[] = $sub;
+	        return $sub;
+    	}
+
+    	/*var_dump($this->subjects);
+    	die;*/
+        
+    }
+
+    public function addStudentOnSubject(string $subjectCode, Student $student){
+
+    	foreach($this->subjects as $s){
+	    	if($s->getCode() == $subjectCode ){
+			   return $s->addStudent($student->getName(), $student->getStuNum());
+			}
+		}
+
+    }
+    public function getStudentsForSubject(string $subjectCode){
+
+    	foreach($this->subjects as $s){
+    		if($s->getCode() == $subjectCode ){
+    			return $s->getStudents();
+			}
+			return [];
+    	}
+    }
+    public function getNumberOfStudents(){
+
+    	$total=0;
+    	foreach($this->subjects as $s){
+    		foreach($s->getStudents() as $st){
+    			$total++;
+    		}
+		}
+		echo '---------------------------------'."\n";
+		return "The student total: ". $total."\n";
+		echo '---------------------------------'."\n";
+
+    }
+    public function print(){
+
+    	foreach($this->subjects as $s){
+			echo '---------------------------------'."\n";
+			echo $s->getCode() .'-'. $s->getName() ."\n";
+			echo '---------------------------------'."\n";
+			
+    		foreach($s->getStudents() as $st){
+				echo $st->getName() ."-". $st->getStuNum();
+				echo "\n";
+    		}
+    	}
+    }
+}
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once "Student.php";
+require_once "Subject.php";
+require_once "University.php";
+
+$university = new University();
+$subject = $university->addSubject('112', 'Web II');
+$subjectTest = $university->addSubject('101', 'CS');
+$subject->addStudent('Jane', '520');
+//$subject->addStudent('Jane', '520');
+//$university->print();
+
+//$subject = new Subject('112', 'Web II');
+$subject->addStudent('George', '123');
+$subject->addStudent("Mary", '234');
+$university->addStudentOnSubject('112', new Student('David', '345'));
+$university->addStudentOnSubject('101', new Student('John', '300'));
+$university->addStudentOnSubject('101', new Student('Ivy', '564'));
+$subject2 = $university->addSubject('113', 'Web III');
+$subject2->addStudent('Bob', '456');
+$subject2->addStudent('Brad', '567');
+echo $university->getNumberOfStudents();
+$university->print(); 
+
+
+
+//print_r($subject);
+//die;
+/**
+* This method must print all subjects and for each subject all students registered on the subject int the following format
+* SubjectCode - SubjectName
+* -------------------------
+* StudentName - StudentNumber
+* Student2Name - Student2Number
+* ... // and so on...
+*
+* Subject2Code - Subject2Name
+* --------------------
+* StudentName - StudentNumber
+* Student2Name - Student2Number
+* ... // and so on...
+* 
+*/ 
+/*
+112 - Web II
+--------------------
+George - 123
+Mary - 234
+David - 345
+
+113 - Web III
+--------------------
+Bob - 456
+Brad - 567
+*/
